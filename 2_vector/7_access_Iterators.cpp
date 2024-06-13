ea85880ab8564af591c9552433b7e7b3c78ac166
Mahbub Hasaan Riyad <riyad.info1@gmail.com>
2024-06-13 20:17:21 +0600

    .

--- 2_vector/7_access_Iterators.cpp
+++ 2_vector/7_access_Iterators.cpp
@@ -1,18 +1,18 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
-    vector<int> v={1,2,3,4,5};
+    vector<int> v = {1, 2, 3, 4, 5};
 
-    //1 cout<<v[v.size()-1];
-    //2 cout<<v.back();
+    // 1 cout<<v[v.size()-1];
+    // 2 cout<<v.back();
 
-    //1 cout<<v[0];
-    //2 cout<<v.front();
+    // 1 cout<<v[0];
+    // 2 cout<<v.front();
 
-    for(auto it=v.begin();it<v.end();it++)
+    for (auto it = v.begin(); it < v.end(); it++)
     {
-        cout<<*it<<" ";
+        cout << *it << " ";
     }
     return 0;
 }
